769f298264dc3d821fd3cc33b92b88ac71e0d93e
gankuizhi <gankuizhi@126.com>
2020-11-20 16:49:50 +0800

    '提交内容描述‘

---
+++ src/main/java/jenkins.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.logging.Logger;
+
+public class jenkins {
+    public static void main(String[] args) {
+        System.out.print("nice");
+    }
+    public static void selectUser(){
+    }
+}
